bf8c915a78e0e7d66aca64940acd122212bb5e34
jeanromeokoffi12-blip <jeanromeokoffi12@gmail.com>
2026-07-09 16:57:11 +0000

    Update parseJourEnDate.js

--- parseJourEnDate.js
+++ parseJourEnDate.js
@@ -1,1 +1,14 @@
 
+function parseJourEnDate(jourStr) {
+  // Ex: "Mardi 07/07" -> "2026-07-07"
+  const match = jourStr.match(/(\d{2})\/(\d{2})/);
+  if (!match) return null;
+
+  const jour = match[1];
+  const mois = match[2];
+  const anneeActuelle = new Date().getFullYear();
+
+  return `${anneeActuelle}-${mois}-${jour}`;
+}
+
+module.exports = parseJourEnDate;
